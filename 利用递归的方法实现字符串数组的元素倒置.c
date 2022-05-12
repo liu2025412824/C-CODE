35aa1bcb8f61b84daabaddc7cf010d3608ae3af2
liu2025412824 <103622223+liu2025412824@users.noreply.github.com>
2022-05-12 13:06:56 +0800

    利用递归的方法实现字符串数组的元素倒置

---
+++ 利用递归的方法实现字符串数组的元素倒置.c
@@ -0,0 +1,31 @@
+//利用递归的方法实现字符串数组的元素倒置
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+void reverse_string(char* arr)
+{
+	char tmp = arr[0];
+	int len = my_strlen(arr);
+	arr[0] = arr[len - 1];
+	arr[len - 1] = '\0';
+	if (my_strlen(arr + 1) >= 2)
+	{
+		reverse_string(arr + 1);
+	}
+	arr[len - 1] = tmp;
+}
+int main()
+{
+	char arr[] = "abcdef";
+	reverse_string(arr);
+	printf("%s\n", arr);
+	return 0;
+}
