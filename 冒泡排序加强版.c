ed558780bc2e6643cc395dae5cf1ad925ef3581b
liu2025412824 <103622223+liu2025412824@users.noreply.github.com>
2022-05-21 09:52:02 +0800

    冒泡排序加强版

---
+++ 冒泡排序加强版.c
@@ -0,0 +1,70 @@
+//冒泡排序加强版
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+};
+void Swap(char* buf1, char* buf2, int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char tmp=*buf1;
+		*buf1 = *buf2;
+		*buf2 = tmp;
+		buf1++;
+		buf2++;
+	}
+}
+void bubble_sort(void* base, int sz, int width,int (*cmp)(void* e1,void* e2))
+{
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
+			{
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+			}
+		}
+	}
+}
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+int cmp_stu_by_age(const void* e1, const void* e2)
+{
+	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+}
+int cmp_stu_by_name(const void* e1, const void* e2)
+{
+	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+}
+void test1()
+{
+	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
+void test2()
+{
+	struct Stu s[3] = { {"zhangsan",30},{"lisi",20},{"wangwu",10} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	//bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+	bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_name);
+}
+int main()
+{
+	//test1();
+	test2();
+	return 0;
+}
