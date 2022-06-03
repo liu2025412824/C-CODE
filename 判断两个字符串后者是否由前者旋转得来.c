9b6016c4b44e0d3818ea612378c1bd30c2c98eb8
liu2025412824 <103622223+liu2025412824@users.noreply.github.com>
2022-06-03 21:09:54 +0800

    判断两个字符串后者是否由前者旋转得来

---
+++ 判断两个字符串后者是否由前者旋转得来.c
@@ -0,0 +1,51 @@
+//判断两个字符串后者是否由前者旋转得来
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+void reverse(char* left, char* right)
+{
+	assert(left != NULL);
+	assert(right != NULL);
+	while (left < right)
+	{
+		int tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+}
+void left_move(char* arr, int k)
+{
+	assert(arr != NULL);
+	int len = strlen(arr);
+	assert(k <= len);
+
+	reverse(arr, arr + k - 1);//逆序左边
+	reverse(arr + k, arr + len - 1);//逆序右边
+	reverse(arr, arr + len - 1);//整体逆序
+}
+int is_left_move(char* s1, char* s2)
+{
+	int len = strlen(s1);
+	int i = 0;
+	for (i = 0; i < len; i++)
+	{
+		left_move(s1, 1);
+		int ret = strcmp(s1, s2);
+		if (ret == 0)
+			return 1;
+	}
+	return 0;
+}
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "cdefab";
+	int ret = is_left_move(arr1, arr2);
+	if (ret == 1)
+		printf("YES");
+	if (ret == 0)
+		printf("NO");
+	return 0;
+}
