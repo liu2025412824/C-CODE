9ed1352b80ff42abeadcfb5651301f3208c22935
liu2025412824 <103622223+liu2025412824@users.noreply.github.com>
2022-06-16 09:57:05 +0800

    面积周长和体积的计算.c

---
+++ 面积周长和体积的计算.c
@@ -0,0 +1,27 @@
+////求三角形的面积和周长
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	double a;
+//	double b;
+//	double c;
+//	scanf("%lf%lf%lf", &a, &b, &c);
+//	double d = (a + b + c) / 2;
+//	double s = sqrt(d * (d - a) * (d - b) * (d - c));
+//	printf("circumference=%.2lf area=%.2lf", d * 2, s);
+//	return 0;
+//}
+
+
+////求球的体积
+//#include<stdio.h>
+//int main()
+//{
+//	double pi = 3.1415923;
+//	double r;
+//	scanf("%lf", &r);
+//	double v = 4.0 / 3 * pi * r * r * r;//一定要用4.0除3，否则4/3的商为1，而不是一点几！
+//	printf("%.3lf", v);
+//	return 0;
+//}
